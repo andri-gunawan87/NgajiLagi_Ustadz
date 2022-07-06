966dcfca4a0dc2ef407d73aa18db8e1154dc6770
andri-gunawan87 <andri.gunawan.a87@gmail.com>
2022-07-06 19:58:04 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NgajiLagi_Ustadz;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NgajiLagi_Ustadz;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
